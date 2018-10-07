b20738c2214fa4c6ae9f6d27625bb3dc7c541701
Eugeniu Marinescu <marinescuEvghenii@gmail.com>
2018-10-08 01:48:45 +0200

    Draggable compoents code example jsfiddle.

--- src/scripts/pages/draggable.js
+++ src/scripts/pages/draggable.js
@@ -2,5 +2,5 @@ require('@styles/pages/draggable.scss');
 require('@templates/pages/draggable.pug');
 require('prismjs/themes/prism-tomorrow.css');
 
-import Prism from 'prismjs';
-Prism.highlightElement(document.getElementById('code'));
+// import Prism from 'prismjs';
+// Prism.highlightElement(document.getElementById('code'));
